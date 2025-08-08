72d8988bef8bb3a1ec2849e23f7466e083bc2cbb
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-08-08 19:24:50 +0600

    server connected to mongodb done

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.VITE_USERNAME}:${process.env.VITE_PASSWORD}@meshal10613.mbbtx0s.mongodb.net/?retryWrites=true&w=majority&appName=meshal10613`;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+// my-hostel
+// iwS5cK6448R1sXNW
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get("/", async(req, res) => {
     res.send("Server is running successfully...");
 });
+
+
+// Connect the client to the server	(optional starting in v4.7)
+// await client.connect();
+// Send a ping to confirm a successful connection
+client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
