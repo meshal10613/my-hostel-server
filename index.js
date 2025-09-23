caf07e145269d6d6fb5f784fb07bd5efcb28d9f1
meshal10613 <syedmohiuddinmeshal@gmail.com>
2025-09-23 09:44:11 +0600

    prisma installed but not added yet

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const app = express();
 const port = process.env.port || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
